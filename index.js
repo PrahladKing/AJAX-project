25d13bef5b24750e8ba3b637d270d4508b354bc5
PrahladKing <phaniprahlad@gmail.com>
2022-09-24 19:50:40 +0530

    ajax country flag project

---
+++ index.js
@@ -0,0 +1,18 @@
+let xhr = new XMLHttpRequest()
+xhr.open('GET', 'https://restcountries.com/v2/all', true)
+xhr.onload = function() {
+    let names = document.getElementById('name')
+    let flag = document.getElementById('flag')
+    let country = JSON.parse(this.response)
+    let name1 = country.map(x => {
+        return x.name
+    })
+    for (x of name1) {
+        names.innerHTML += `<option value="${x}"> ${x} </option>` 
+    }
+    let flag1 = country.filter(y => {
+        return (names.value == y.name)
+    })
+    flag.innerHTML = `<img src="${flag1[0].flag}">`
+}
+xhr.send()
